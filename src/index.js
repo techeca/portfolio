04f7cb680228f27262f04f745feafcf6c78f3df0
techeca <jp.vasquez@outlook.com>
2022-10-14 20:45:26 -0300

    up

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'
+import { hydrate, render } from 'react-dom'
 import './index.css';
 import 'flowbite';
 //import App from './App';
 import Home from 'components/pages/Home'
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = document.getElementById('root');
+if(root.hasChildNodes()){
+  hydrate(<Home />, root)
+} else {
+  render(<Home />, root)
+}
 
-    <Home />
-
-);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
